4986a2107e0b92e3987c6b28906bc9bf6d93c2c5
austieoduor <austineoduor57@gmail.com>
2022-12-20 08:40:58 +0000

    file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-  }
-
-  return [task, task2];
+	task = false;
+	task2 = true;
+	if (trueOrFalse) {
+		const task;
+		const task2;
+	}
+	return [task, task2];
 }
