8e634be08d9474a8a797512e37f8c7a4f9221790
austineoduor <austineoduor57@gmail.com>
2023-03-14 11:38:41 +0000

    file

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+const bodyParser = require('body-parser');
+
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
